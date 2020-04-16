6e34e0846d247ab5b990a3ae55b4fb8c105d9472
stephen pesce <stevepesce879@gmail.com>
2020-04-16 23:16:44 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,8 +1,9 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
+  if
+    string[0] == "s"
+    string = "ssssssssss#{string}"
   else
     string
   end
