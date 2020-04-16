dfabb6450e9376650d5dc82740cc182fd2a104af
stephen pesce <stevepesce879@gmail.com>
2020-04-16 23:13:26 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,17 +1,10 @@
 # don't forget to add: require 'pry'
 
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
-  else
-    string
-  end
-end
-
-def snake_it_up(string)
   if
     string[0] == "s"
     string = "ssssssssss#{string}"
-    end
+  else
+    string
   end
 end
